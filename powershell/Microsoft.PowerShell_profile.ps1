d65a6fc97d516b61a60dc95103734591bd851b76
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2024-02-22 11:16:52 -0600

    powershell custom alias

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,39 @@
+function Show-As-Linux {
+    Get-ChildItem -Attributes ReadOnly, Hidden, Directory, Archive, Device, Normal, Temporary, SparseFile, ReparsePoint, Compressed, Offline, NotContentIndexed, Encrypted, IntegrityStream, NoScrubData | ForEach-Object {
+        $color = "White"
+
+        # Change color based on attribute
+        if ($_.Attributes -match 'Directory') { $color = "DarkCyan" }
+        elseif ($_.Attributes -match 'Hidden') { $color = "DarkGray" }
+        elseif ($_.Attributes -match 'ReadOnly') { $color = "Gray" }
+        elseif ($_.Attributes -match 'Compressed') { $color = "Blue" }
+
+        # Prepare the output string
+        $output = "$($_.LastWriteTime)  $($_.Mode)  $([string]::Format("{0,10:N0}", $_.Length))  $_"
+
+        # Write the output with color
+        Write-Host $output -ForegroundColor $color
+    }
+}
+
+function Show-All-Files {
+    Get-ChildItem -Force -Attributes ReadOnly, Hidden, System, Directory, Archive, Device, Normal, Temporary, SparseFile, ReparsePoint, Compressed, Offline, NotContentIndexed, Encrypted, IntegrityStream, NoScrubData | ForEach-Object {
+        $color = "White"
+
+        # Change color based on attribute
+        if ($_.Attributes -match 'Directory') { $color = "DarkCyan" }
+        elseif ($_.Attributes -match 'Hidden') { $color = "DarkGray" }
+        elseif ($_.Attributes -match 'System') { $color = "Red" }
+        elseif ($_.Attributes -match 'ReadOnly') { $color = "Gray" }
+        elseif ($_.Attributes -match 'Compressed') { $color = "Blue" }
+
+        # Prepare the output string
+        $output = "$($_.LastWriteTime)  $($_.Mode)  $([string]::Format("{0,10:N0}", $_.Length))  $_"
+
+        # Write the output with color
+        Write-Host $output -ForegroundColor $color
+    }
+}
+
+Set-Alias -Name l -Value Show-As-Linux
+Set-Alias -Name la -Value Show-All-Files
